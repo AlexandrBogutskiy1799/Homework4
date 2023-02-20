5e2dbf39ca6b0ad747e11a2d7a94a5172a24d046
BogustkiyAlexandr1799 <bogutskiy1799@ukr.netgit config --global user.email git config --global user.email bogutskiy1799@ukr.net>
2023-02-20 17:36:59 +1300

    changes

--- src/Homework5/BookingComAPI.java
+++ src/Homework5/BookingComAPI.java
@@ -1,10 +1,11 @@
 package Homework5;
 
-public class BookingComAPI implements API{
+public class BookingComAPI implements API {
     @Override
     public Room[] findRooms(int price, int persons, String city, String hotel) {
         return new Room[0];
     }
+
     Room[] rooms; //В каждой реализации есть глобальная переменная Room[]
     // rooms, которая должна быть инициализована констуктором по умолчанию пятью разными
     //объектами Room.
---
+++ src/Homework5/Controller.java
@@ -0,0 +1,5 @@
+package Homework5;
+
+public class Controller {
+    API apis[] = new API[3];
+}
