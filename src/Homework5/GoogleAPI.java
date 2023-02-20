ac360282be92881c5b0bd03d0efa799d0e452eee
BogustkiyAlexandr1799 <bogutskiy1799@ukr.netgit config --global user.email git config --global user.email bogutskiy1799@ukr.net>
2023-02-20 17:32:32 +1300

    changes

---
+++ src/Homework5/GoogleAPI.java
@@ -0,0 +1,9 @@
+package Homework5;
+
+public class GoogleAPI implements API{
+    @Override
+    public Room[] findRooms(int price, int persons, String city, String hotel) {
+        return new Room[0];
+    }
+    Room[] rooms;
+}
---
+++ src/Homework5/TripAdvisorAPI.java
@@ -0,0 +1,9 @@
+package Homework5;
+
+public class TripAdvisorAPI implements API{
+    @Override
+    public Room[] findRooms(int price, int persons, String city, String hotel) {
+        return new Room[0];
+    }
+    Room[] rooms;
+}
